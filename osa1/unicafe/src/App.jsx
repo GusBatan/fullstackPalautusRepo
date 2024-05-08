b9eebc92133c125179906cd1b5c22f9b1d562477
Tuomas <register@auroracode.fi>
2024-05-08 12:15:22 +0300

    complete unicafe

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,52 +1,55 @@
+import { useState } from 'react';
+
+const StatisticsLine = ({ label, value }) => {
+  return (
+    <tr>
+      <td>{label}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>No feedback given</h2>
+      </div>
+    );
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine label={'good'} value={good} />
+        <StatisticsLine label={'neutral'} value={neutral} />
+        <StatisticsLine label={'bad'} value={bad} />
+        <StatisticsLine label={'average'} value={(good - bad) / total} />
+        <StatisticsLine label={'positive'} value={(good / total) * 100} />
+      </tbody>
+    </table>
+  );
+};
+
+const MyButton = ({ action, label }) => {
+  return <button onClick={action}>{label}</button>;
+};
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
-
-  const Header = ({ course }) => <h1>{course.name}</h1>;
-
-  const Content = ({ course }) => (
-    <>
-      <p>
-        {course.parts[0].name} {course.parts[0].exercises}
-      </p>
-      <p>
-        {course.parts[1].name} {course.parts[1].exercises}
-      </p>
-      <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-      </p>
-    </>
-  );
-
-  const Total = ({ course }) => (
-    <p>
-      Number of exercises{' '}
-      {course.parts[0].exercises +
-        course.parts[1].exercises +
-        course.parts[2].exercises}
-    </p>
-  );
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h2>give feedback</h2>
+      <MyButton label={'good'} action={() => setGood(good + 1)} />
+      <MyButton label={'neutral'} action={() => setNeutral(neutral + 1)} />
+      <MyButton label={'bad'} action={() => setBad(bad + 1)} />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
