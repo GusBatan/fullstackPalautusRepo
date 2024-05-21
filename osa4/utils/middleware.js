40efa589c7bdffab7ee12f8c39f3fdd17f782413
Tuomas <register@auroracode.fi>
2024-05-21 13:38:48 +0300

    fix exprired token bug

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -2,16 +2,23 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const errorHandler = (error, request, response, next) => {
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
+  }
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'Cast Error' });
-  } else if (error.name === 'ValidationError') {
+  } 
+  if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
-  } else if (error.name === 'JsonWebTokenError') {
+  }  
+  if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'token missing or invalid' });
-  } else if (
+  }  
+  if (
     error.name === 'MongoServerError' &&
     error.message.includes('E11000 duplicate key error')
-  ) {
+  ) 
+  {
     return response
       .status(400)
       .json({ error: 'expected `username` to be unique' });
@@ -34,16 +41,19 @@ const userExtractor = async (req, res, next) => {
   if (!token) {
     return res.status(401).json({ error: 'token missing' });
   }
+  try {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
 
-  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: 'token invalid' });
+    }
 
-  if (!decodedToken.id) {
-    return res.status(401).json({ error: 'token invalid' });
+    req.user = await User.findById(decodedToken.id);
+
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  req.user = await User.findById(decodedToken.id);
-
-  next();
 };
 
 module.exports = { errorHandler, tokenExtractor, userExtractor };
